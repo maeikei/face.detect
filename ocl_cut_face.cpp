02049d0baa78c7ae1dfab5202dd84f35f753d76f
Wator Vapor <watorvapor@gmail.com>
2015-08-20 20:26:44 +0900

    data

--- ocl_cut_face.cpp
+++ ocl_cut_face.cpp
@@ -24,7 +24,7 @@ const static string cStrFaceCascadeName =
 static int faceSize = 256;
 static bool faceGray = false;
 static int constSkipFrontFaceCounter = 24*5;
-static int constTotalFaceCounter = 24*30;
+static int constTotalFaceCounter = 24*60;
 
 
 
